163630ed200d78c8219d574498a9034543147194
gk-svg <gk32239@gmail.com>
2020-06-17 23:44:09 +0530

    css/images  added

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request,'myapp/index.html')
+    return render(request,'myapp/base.html')
