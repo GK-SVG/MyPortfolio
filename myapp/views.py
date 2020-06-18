f29e700e8d410d8d0b46cc0e196d063805b7e94f
gk-svg <gk32239@gmail.com>
2020-06-18 23:49:08 +0530

    css for responsive site

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request,'myapp/base.html')
+    return render(request,'myapp/index.html')
+
+def about(request):
+    return  render(request,'myapp/about.html')
