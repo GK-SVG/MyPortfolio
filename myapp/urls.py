4bf775acfb278df305318965f363b3dde3d7b924
gk-svg <gk32239@gmail.com>
2020-06-19 15:56:49 +0530

    urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,6 @@ from .import views
 urlpatterns = [
     path('',views.home,name='Home'),
     path('about/',views.about,name='About'),
+    path('project/',views.project,name='Project'),
+    path('certificate/',views.certificate,name='Certificate'),   
 ]
