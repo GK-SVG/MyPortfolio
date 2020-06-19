373fe7bdad8850f876bd81e11f2030cdee15aa3a
gk-svg <gk32239@gmail.com>
2020-06-19 15:57:41 +0530

    templates/static files

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render,HttpResponse
-
+from .models import *
 # Create your views here.
 def home(request):
     return render(request,'myapp/index.html')
 
 def about(request):
-    return  render(request,'myapp/about.html')
+    skills=Skill.objects.all()
+    return  render(request,'myapp/about.html',{'skills':skills})
+
+def project(request):
+    projects=Project.objects.all()
+    return render(request,'myapp/project.html',{'projects':projects})
+
+def certificate(request):
+    certificates=Certificate.objects.all()
+    return render(request,'myapp/certificate.html',{'certificates':certificates})
