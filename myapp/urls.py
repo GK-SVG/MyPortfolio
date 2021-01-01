ac50b3c1af2729c84606802080e5adf8e4af3c68
gk-svg <gk32239@gmail.com>
2021-01-01 23:24:49 +0530

    my Portfolio

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,7 @@ from .import views
 
 urlpatterns = [
     path('',views.home,name='Home'),
-    path('about/',views.about,name='About'),
-    path('project/',views.project,name='Project'),
-    path('certificate/',views.certificate,name='Certificate'),   
+    # path('about/',views.about,name='About'),
+    # path('project/',views.project,name='Project'),
+    # path('certificate/',views.certificate,name='Certificate'),   
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,19 +5,22 @@ from django.http import JsonResponse
 # Create your views here.
 def home(request):
     mycv = MyCV.objects.all()[0]
-    return render(request,'myapp/index.html',{'mycv':mycv})
-
-def about(request):
-    mycv = MyCV.objects.all()[0]
     skills=Skill.objects.all()
-    return  render(request,'myapp/about.html',{'skills':skills,'mycv':mycv})
-
-def project(request):
-    mycv = MyCV.objects.all()[0]
     projects=Project.objects.all()
-    return render(request,'myapp/project.html',{'projects':projects,'mycv':mycv})
-
-def certificate(request):
-    mycv = MyCV.objects.all()[0]
     certificates=Certificate.objects.all()
-    return render(request,'myapp/certificate.html',{'certificates':certificates,'mycv':mycv})
+    return render(request,'myapp/index.html',{'mycv':mycv,'skills':skills,'projects':projects,'certificates':certificates})
+
+# def about(request):
+#     mycv = MyCV.objects.all()[0]
+#     skills=Skill.objects.all()
+#     return  render(request,'myapp/about.html',{'mycv':mycv})
+
+# def project(request):
+#     mycv = MyCV.objects.all()[0]
+#     projects=Project.objects.all()
+#     return render(request,'myapp/project.html',{'mycv':mycv})
+
+# def certificate(request):
+#     mycv = MyCV.objects.all()[0]
+#     certificates=Certificate.objects.all()
+#     return render(request,'myapp/certificate.html',{'certificates':certificates,'mycv':mycv})
